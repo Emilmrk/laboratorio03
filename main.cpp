94b2628cb01f85883ff976636ca082ac012250cb
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2024-09-18 09:22:44 -0600

    Update main.cpp
    primera entrega de metodos

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ public:
 { return ""; /* TODO */ }
     string createHeader(const string& header, int level) 
 { return ""; /* TODO */ }
-    string createParagraph(const string& text) 
-{ return ""; /* TODO */ }
-    string createLink(const string& url, const string& text) 
-{ return ""; /* TODO */ }
-    string createList(const vector<string>& items, bool ordered) 
-{ return ""; /* TODO */ }
-};
+    string createParagraph(const string& text)
+{ return "<p>".text."<p>"; /* TODO */ }
+    string createLink(const string& url, const string& text)
+{ return "<a href=".url.">".text."<a>"; /* TODO */ }
+    string createList(const vector<string>& items, bool ordered)
+        {
+        string listaC = "<ol>\n";
+        for(string cadena: items){
+                listaC = listaC."<li>".cadena."<li>\n";
+        }
+        listaC = listaC."<ol>";
+        return ""; /* TODO */ }
+        };
